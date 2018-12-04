074905a0a33dad633db97a0e9b4c8c44835ad3b4
Bryan Deloeste <bryandeloeste@gmail.com>
2018-12-04 16:01:11 -0500

    gofmt

--- day1-1.go
+++ day1-1.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	"bufio"
-  "fmt"
-  "log"
+	"fmt"
+	"log"
 	"os"
-  "strconv"
+	"strconv"
 )
 
 func main() {
-  cwd, _ := os.Getwd()
-  file, err := os.Open(cwd + "\\data\\1")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
+	cwd, _ := os.Getwd()
+	file, err := os.Open(cwd + "\\data\\1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  i := 0
-  for scanner.Scan() {
-    t := scanner.Text()
-    if s, err := strconv.Atoi(t); err == nil {
-      i += s
-    }
-  }
+	scanner := bufio.NewScanner(file)
+	i := 0
+	for scanner.Scan() {
+		t := scanner.Text()
+		if s, err := strconv.Atoi(t); err == nil {
+			i += s
+		}
+	}
 
-  fmt.Println(i)
+	fmt.Println(i)
 
-  if err := scanner.Err(); err != nil {
-    log.Fatal(err)
-  }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
